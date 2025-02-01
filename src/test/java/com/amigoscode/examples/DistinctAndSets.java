458a45afaed460d4cb033ffda35581ca7fcc5400
Emin Orçun SANCAR <eorcunsancar@gmail.com>
2025-02-01 21:14:46 +0300

    Distinct list

--- src/test/java/com/amigoscode/examples/DistinctAndSets.java
+++ src/test/java/com/amigoscode/examples/DistinctAndSets.java
@@ -4,12 +4,18 @@ package com.amigoscode.examples;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class DistinctAndSets {
 
     @Test
     public void distinct() throws Exception {
         List<Integer> numbers = List.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
+        List<Integer> distinct = numbers.stream().distinct().collect(Collectors.toList());
+        assertThat(distinct).hasSize(9);
+        System.out.println(distinct);
     }
 
     @Test
