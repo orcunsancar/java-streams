16f0598704a0e77b41e7e119dab76e99c2aeb800
Emin Orçun SANCAR <eorcunsancar@gmail.com>
2025-02-01 21:00:30 +0300

    InStreams range example

--- src/test/java/com/amigoscode/examples/IntStreams.java
+++ src/test/java/com/amigoscode/examples/IntStreams.java
@@ -6,11 +6,21 @@ import com.amigoscode.mockdata.MockData;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class IntStreams {
 
     @Test
     public void range() throws Exception {
+        System.out.println("with fori");
+        for (int i = 0; i <=10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("with int stream exclusive");
+        IntStream.range(0, 10).forEach(System.out::println);
+
+        System.out.println("with int stream inclusive");
+        IntStream.rangeClosed(0, 10).forEach(System.out::println);
     }
 
     // Loop through people using IntStream
