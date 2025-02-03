c3715e2769f5314a2f0c608938f95fb475a634f9
Emin Orçun SANCAR <eorcunsancar@gmail.com>
2025-02-03 19:44:35 +0300

    take while example

--- src/test/java/com/amigoscode/examples/Filtering.java
+++ src/test/java/com/amigoscode/examples/Filtering.java
@@ -43,6 +43,8 @@ public class Filtering {
 
         System.out.println();
         System.out.println("using take while");
+        Stream.of(2, 4, 6, 8, 9, 10, 12).takeWhile(n -> n % 2 == 0)
+                .forEach(n -> System.out.print(n + " "));
     }
 
     @Test
