8106c3cd731571093409b305a235524a328a1bdf
Emin Orçun SANCAR <eorcunsancar@gmail.com>
2025-02-03 21:08:42 +0300

    map example

--- src/test/java/com/amigoscode/examples/TransformationsMapAndReduce.java
+++ src/test/java/com/amigoscode/examples/TransformationsMapAndReduce.java
@@ -2,17 +2,32 @@ package com.amigoscode.examples;
 
 import com.amigoscode.beans.Car;
 import com.amigoscode.beans.Person;
+import com.amigoscode.beans.PersonDTO;
 import com.amigoscode.mockdata.MockData;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class TransformationsMapAndReduce {
 
     @Test
     void yourFirstTransformationWithMap() throws IOException {
         List<Person> people = MockData.getPeople();
+
+        Function<Person, PersonDTO> personPersonDTOFunction = person -> new PersonDTO(
+                person.getId(),
+                person.getFirstName(),
+                person.getAge());
+
+        List<PersonDTO> dtos = people.stream()
+                .filter(person -> person.getAge() > 28)
+                .map(PersonDTO::map)
+                .collect(Collectors.toList());
+
+        dtos.forEach(System.out::println);
     }
 
     @Test
