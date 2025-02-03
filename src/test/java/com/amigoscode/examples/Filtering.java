2360c4a5002b1f7c29b549a19228a37147a3f506
Emin Orçun SANCAR <eorcunsancar@gmail.com>
2025-02-03 19:46:33 +0300

    drop while example

--- src/test/java/com/amigoscode/examples/Filtering.java
+++ src/test/java/com/amigoscode/examples/Filtering.java
@@ -32,6 +32,8 @@ public class Filtering {
                 .forEach(n -> System.out.print(n + " "));
         System.out.println();
         System.out.println("using dropWhile");
+        Stream.of(2, 4, 6, 8, 9, 10, 12).dropWhile(n -> n % 2 == 0)
+                .forEach(n -> System.out.print(n + " "));
     }
 
     @Test
