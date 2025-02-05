fd45356bf73d75af1f823ad97d2c37758093fb5f
Emin Orçun SANCAR <eorcunsancar@gmail.com>
2025-02-05 19:09:12 +0300

    group by example

--- src/test/java/com/amigoscode/examples/GroupingData.java
+++ src/test/java/com/amigoscode/examples/GroupingData.java
@@ -6,12 +6,22 @@ import com.amigoscode.mockdata.MockData;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class GroupingData {
 
     @Test
     public void simpleGrouping() throws Exception {
         List<Car> cars = MockData.getCars();
+        Map<String, List<Car>> map = cars
+                .stream()
+                .collect(Collectors.groupingBy(Car::getMake));
+        map.forEach((s, carss) -> {
+            System.out.println("Make " + s);
+            carss.forEach(System.out::println);
+            System.out.println("---------------------");
+        });
     }
 
     @Test
